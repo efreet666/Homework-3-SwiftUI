d5a468ef01b9e956ad2f299910b1c65b1c661a18
Vlad <vladislav.bok@yandex.ru>
2022-05-11 22:39:43 +0300

    Initial Commit

---
+++ Homework 3 SwiftUI/Homework_3_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_3_SwiftUIApp.swift
+//  Homework 3 SwiftUI
+//
+//  Created by Влад Бокин on 11.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Homework_3_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
